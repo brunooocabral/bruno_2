ad29ca57a8eaa98a71b4c2629c9ffa64b220f808
bruno__6 <162610624+brunooocabral@users.noreply.github.com>
2024-03-07 14:01:46 -0300

    Add files via upload

---
+++ While.py
@@ -0,0 +1,25 @@
+#-Imprimir los números del 1 al 500
+x=1
+while   x<=500:
+    print(x)
+    x=x+1
+#Imprimir los número del 50 al 100
+x=50
+while   x<=100:
+    print(x)
+    x=x+2
+#Imprimir los número del -50 al 100
+x=-50
+while   x<=0:
+    print(x)
+    x=x+1
+#Imprimir los número del 2 al 100
+x=2
+while   x<=100:
+    print(x)
+    x=x+1
+#Inicializar a x con el valor 2 y dentro del bloque repetitivo invrementar x a 2
+x=2
+while   x<=100:
+    print(x)
+    x=x+2
